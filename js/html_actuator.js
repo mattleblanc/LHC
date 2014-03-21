256e916623cc2ccbe7cc7e28f774610afefe5160
mattleblanc <matthewleblanc@gmail.com>
2014-03-20 19:00:58 -0700

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -141,7 +141,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win a Nobel Prize!!" : "Too much pileup! Try again!";
+  var message = won ? "You win a Nobel Prize!!" : "Too much pileup!";
   
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
